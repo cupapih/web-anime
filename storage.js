32ff3dae7a533e1eaf9a9e0fd5b9950424dd7c79
cupapih <seins3002@gmail.com>
2026-01-08 21:46:08 +0700

    commit

--- storage.js
+++ storage.js
@@ -1,20 +1,19 @@
-const DB_KEY = 'BSTATIONX_DB';
+const DB_KEY = "BSTATIONX_DB";
 
-function getDB(){
+function getDB() {
   return JSON.parse(localStorage.getItem(DB_KEY)) || [];
 }
 
-function saveDB(data){
+function saveDB(data) {
   localStorage.setItem(DB_KEY, JSON.stringify(data));
 }
 
-function addAnime(anime){
+function clearDB() {
+  saveDB([]);
+}
+
+function addAnime(anime) {
   const db = getDB();
   db.push(anime);
   saveDB(db);
 }
-
-function clearDB(){
-  localStorage.removeItem(DB_KEY);
-}
-
